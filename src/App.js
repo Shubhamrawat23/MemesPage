1abe2d394197703eb9aa8ec6c12575b154d4235d
Shubhamrawat23 <shubhamrawat2311@gmail.com>
2024-03-12 13:57:06 +0530

    add loading feature and css in recent searches section

--- src/App.js
+++ src/App.js
@@ -1,65 +1,120 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
 import './App.css';
 import useGetMeme from './C_Hook/useGetMeme.js'
 import InputFields from './Components/InputFields.js';
 import MemeCard from './Components/MemeCard/MemeCard.js'
 import BackBtn from "./Icons/back-animation.mp4"
+import BackPoster from "./Icons/left-arrow.png"
+import NextBtn from "./Icons/next-animation.mp4"
+import NextPoster from "./Icons/right-arrow.png"
+
 
 function App() {
-  const [page,setPage] = useState(1)
   const [name,setName] = useState("")
+  const [page,setPage] = useState(1)
   const [recentSearch,setRecentSearch] = useState([])
-  const getMeme = useGetMeme(name)
+  const [isLoading,setIsLoading] = useState(false)
+  const getMeme = useGetMeme(name,setIsLoading,setPage)
 
 
-  const handleChannelName = (e)=>{
+  const handleChannelName = (e) => {
     e.preventDefault()
-    if (e.target.channelName.value !=="") {
+    if (e.target.channelName.value !== "") {
       let valueOfName = e.target.channelName.value
       setName(valueOfName)
-      if (!recentSearch.includes(valueOfName)) {setRecentSearch([...recentSearch,valueOfName])
-      
-      localStorage.setItem("recentSearch",JSON.stringify([valueOfName,...recentSearch]))}
+      setIsLoading(true)
+      if (!recentSearch.includes(valueOfName)) {
+        setRecentSearch([...recentSearch, valueOfName])
+        
+        localStorage.setItem("recentSearch", JSON.stringify([valueOfName, ...recentSearch]))
+      }
     }
-  else{
-    return alert("please enter some channel name")
-  }    
-  e.target.channelName.value=""
-  
-}
+    else {
+      return alert("please enter some channel name")
+    }
+    e.target.channelName.value = ""
+  }
 
-
-console.log(getMeme);
+  console.log(getMeme);
 
 useEffect(()=>{
-  setPage(1)
   let prevSearches = JSON.parse(localStorage.getItem("recentSearch"));
   if(prevSearches !== null) setRecentSearch(prevSearches)
-  },[name,setRecentSearch])
+  },[])
  
   return (
     <div className="App">
+
+      {/*--------------------- Input Channels Name --------------------- */}
       <InputFields onChannelName={handleChannelName} />
-      {getMeme ? `These are ${name} Memes` : `"${name}" Name's Memes Not Found`}
-      <div id='mainMemeContainer'>
-        {getMeme && getMeme.slice(page * 6 - 6, page * 6).map((value, index) => (
-          <div key={index} className='memeBox'>
-            <MemeCard title={value.title} image={value.preview} postPreview={value.postLink} downloadLink={value.url} />
-          </div>
-        ))}
+
+      <div id={name===""?'boxOfAboutMeme':'activeAboutMeme'}>
+        {name === "" ?
+          <div id='enterName'>Please Enter any channel Name</div> :
+          <>
+            {isLoading ? `Finding "${name}" Memes` : (getMeme && !isLoading ? `These are "${name}" Memes` : `"${name}" Name's Memes Not Found`)}
+          </>
+        }
       </div>
 
-      <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Back</button>
-      <button disabled={getMeme && page >= Math.ceil((getMeme.length) / 6)} onClick={() => setPage(page + 1)}>Next</button>
 
 
+      {/*------------------------ Main Meme Card ----------------- */}
+
+      {isLoading ?
+        (<h1 style={{ color: "white" }}>Loading...</h1>) :
+        <div id='mainMemeContainer'>
+          {getMeme && getMeme.slice(page * 6 - 6, page * 6).map((value, index) => (
+            <div key={index} className='memeBox'>
+              <MemeCard title={value.title}
+                image={value.preview}
+                postPreview={value.postLink}
+                downloadLink={value.url} />
+            </div>
+          ))}
+        </div>
+      }
+
+
+
+
+      {/*-------------- Next/Previous Button------------ */}
+      <button disabled={page <= 1 || !getMeme}
+        onClick={() => setPage(prevVal=>prevVal - 1)}
+        id='prevBtn'>
+        <video src={BackBtn}
+          poster={BackPoster}
+          onClick={(e) => e.target.play()}
+          id='prevVideo'
+          disabled={page <= 1} />
+      </button>
+
+      <button disabled={!getMeme || page >= Math.ceil((getMeme.length) / 6)}
+        onClick={() => setPage(prevVal=>prevVal + 1)}
+        id='nextBtn'>
+        <video src={NextBtn}
+          poster={NextPoster}
+          onClick={(e) => e.target.play()}
+          id='nextVideo'
+          disabled={getMeme && page >= Math.ceil((getMeme.length) / 6)} />
+      </button>
+
+
+
+
+      {/*----------------- Recent Searches -------------------- */}
       <div>
-        <p>Recent Searches</p>
-        {recentSearch && recentSearch.map((value, i) => (
-          <div key={i} onClick={() => (setName(value), window.scrollTo({ top: 0, behavior: "smooth" }))}>
-            {value}
-          </div>
-        ))}
+        <p style={{ color: "white", fontSize: "xx-large" }}>Recent Searches</p>
+        <div id='mainRecentSearch'>
+          {recentSearch && recentSearch.map((value, i) => (
+            <div key={i}
+              className='searches'
+              style={{ backgroundColor: value === name ? "wheat" : "black", color: value === name ? "black" : "wheat" }}
+              onClick={() => (setName(value), setIsLoading(true), setPage(1))}>
+              {value}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/C_Hook/useGetMeme.js
+++ src/C_Hook/useGetMeme.js
@@ -1,14 +1,16 @@
 import { useEffect ,useState} from "react";
 
-export default function useGetMeme(memeChannel, noOfMemes=50) {
+export default function useGetMeme(memeChannel,setIsLoading,setPage) {
     const [data,setData] = useState([])
     useEffect(()=>{
         if (memeChannel !== "") {
-            fetch(`https://meme-api.com/gimme/${memeChannel}/${noOfMemes}`)
-            .then((resp)=>resp.json())
-            .then((res)=>setData(res.memes))
-            .catch((e)=>console.log(e))
+            fetch(`https://meme-api.com/gimme/${memeChannel}/50`)
+                .then((resp) => resp.json())
+                .then((res) => (setPage(1),
+                    setData(res.memes),
+                    setIsLoading(false)))
+                .catch((err)=>console.log(err))
         }
-    },[memeChannel,noOfMemes])
+    },[memeChannel,setIsLoading,setPage])
     return data;
 }
